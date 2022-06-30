1d54ac76b9f72490edeedc3e5ad2f94a47a82650
Shivam Goel <Shivam0922@gmail.com>
2022-06-30 19:16:05 -0400

    Added the password label

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,6 +5,9 @@
     </label>
     <input type="email" required v-model = "email"/>
 
+    <label>
+        Password:
+    </label>
     <input type="password" required v-model = "password"/>
 
   </form>
