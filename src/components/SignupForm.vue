367bc56084797075ae13ce77beb4494633e82a6e
Shivam Goel <Shivam0922@gmail.com>
2022-07-02 23:29:37 -0400

    32.39

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -10,7 +10,7 @@
     <input type="role" required v-model="role" />
 
     <label> Skills: </label>
-    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
     <div v-for="skill in skills" :key="skill" class="pill">
       {{ skill }}
     </div>
@@ -67,7 +67,9 @@ export default {
   methods: {
     addSkill(e) {
       if (e.key === "," && this.tempSkill) {
-        this.skills.push(this.tempSkill);
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
         this.tempSkill = "";
       }
     },
@@ -112,5 +114,18 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12 px;
+  background: #eeee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
