f51cb8d4964707f76ed0b37da356ffc2d5ff6964
Shivam Goel <Shivam0922@gmail.com>
2022-07-02 23:06:13 -0400

    Formatted it

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,56 +1,56 @@
 <template>
-  <form>
-    <label>
-        Email:
-    </label>
-    <input type="email" required v-model = "email"/>
+    <form>
+        <label>
+            Email:
+        </label>
+        <input type="email" required v-model="email" />
 
-    <label>
-        Password:
-    </label>
-    <input type="password" required v-model = "password"/>
+        <label>
+            Password:
+        </label>
+        <input type="password" required v-model="password" />
 
-    <label>
-        Role:
-    </label>
-    <input type="role" required v-model = "role"/>
+        <label>
+            Role:
+        </label>
+        <input type="role" required v-model="role" />
 
-    <select>
-        <option value="Developer">Web Developer</option>
-        <option value="Designer">Web Designer</option>
-    </select>
+        <select>
+            <option value="Developer">Web Developer</option>
+            <option value="Designer">Web Designer</option>
+        </select>
 
-    <div class="terms">
-        <input type="checkbox" v-model="terms" required>
-        <label> Accept the terms and conditions</label>
-    </div>
-
-
-    <div>
-        <input type="checkbox" value="Shaun" v-model="names">
-        <label>Shaun</label>
-    </div>
-
-    <div>
-        <input type="checkbox" value="Yoshi" v-model="names">
-        <label>Yoshi</label>
-    </div>
-
-    <div>
-        <input type="checkbox" value="Mario" v-model="names">
-        <label>Mario</label>
-    </div>
+        <div class="terms">
+            <input type="checkbox" v-model="terms" required>
+            <label> Accept the terms and conditions</label>
+        </div>
 
 
+        <div>
+            <input type="checkbox" value="Shaun" v-model="names">
+            <label>Shaun</label>
+        </div>
+
+        <div>
+            <input type="checkbox" value="Yoshi" v-model="names">
+            <label>Yoshi</label>
+        </div>
+
+        <div>
+            <input type="checkbox" value="Mario" v-model="names">
+            <label>Mario</label>
+        </div>
 
 
 
-  </form>
 
-<p> Email: {{ email }}</p>
-<p>Password: {{ password  }}</p>
-<p>Role: {{ role  }}</p>
-<p>Terms Accpeted: {{ terms }}</p>
+
+    </form>
+
+    <p> Email: {{ email }}</p>
+    <p>Password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
+    <p>Terms Accpeted: {{ terms }}</p>
 
 
 
@@ -61,50 +61,53 @@
 <script>
 export default {
     data() {
-        return{
+        return {
             email: 'Mario',
-            password:'',
-            role:'designer',
+            password: '',
+            role: 'designer',
             terms: false,
-            names:[],
+            names: [],
         }
     }
 }
 </script>
 
 <style>
-    form {
-        max-width:420px;
-        margin: 30px auto;
-        background: white;
-        text-align: left;
-        padding: 40px; 
-        border-radius: 10px;
-    }
-    label {
-     color: #aaa;
-     display: inline-block;
-     margin: 25px 0 15 px;
-     font-size: 0.6em;
-     text-transform: uppercase;   
-     letter-spacing: 1px;
-     font-weight: bold;
-    }
-    input, select {
-        display: block;
-        padding: 10px 6px;
-        width: 100%;
-        box-sizing: border-box;
-        border: none;
-        border-bottom: 1px solid #ddd;
-        color: #555;
-    }
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
 
-    input[type="checkbox"]{
-        display: inline-block;
-        width: 16px;
-        margin: 0 10px 0 0;
-        position: relative;
-        top: 2px;
-    }
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15 px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+
+input,
+select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+
+input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+}
 </style>
